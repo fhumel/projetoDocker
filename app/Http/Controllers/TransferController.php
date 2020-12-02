c6399eaea0be06a51814e3397d78213f96f1f877
procopio_artit <fernando.procopio_artit@totalexpress.com.br>
2020-12-01 23:01:42 -0300

    Inicializando Laravel com Docker

---
+++ app/Http/Controllers/TransferController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TransferController extends Controller
+{
+    public function test() {
+       dd('entro na view');
+    }
+}
