d5d99c6553d0deef19c18cdb4d8ff2b0b66ae79c
Fernando Humel <fernando.procopio_artit@totalexpress.com.br>
2020-12-08 20:21:03 -0300

    removendo vendor

--- routes/api.php
+++ routes/api.php
@@ -2,18 +2,11 @@
 
 /** @var \Laravel\Lumen\Routing\Router $router */
 
+$router->post('register', ['uses' => 'Users\UserController@create']);
+$router->get('list', ['uses' => 'Users\UserController@list']);
 
-$router->group(['prefix' => 'user'], function () use ($router) {
-    $router->post('register', ['uses' => 'Users\UserController@create']);
-    $router->get('list', ['uses' => 'Users\UserController@list']);
-});
+$router->post('deposit', ['uses' => 'Users\Wallets\WalletController@deposit']);
+$router->get('balance', ['uses' => 'Users\Wallets\WalletController@balance']);
 
-$router->group(['prefix' => 'wallet'], function () use ($router) {
-    $router->post('deposit', ['uses' => 'Users\Wallets\WalletController@deposit']);
-    $router->get('balance', ['uses' => 'Users\Wallets\WalletController@balance']);
-});
-
-$router->group(['prefix' => 'transactions'], function () use ($router) {
-    $router->post('pay', ['uses' => 'Users\Wallets\Transactions\TransactionController@pay']);
-    $router->get('list', ['uses' => 'Users\Wallets\Transactions\TransactionController@list']);
-});
+$router->post('transaction', ['uses' => 'Users\Wallets\Transactions\TransactionController@pay']);
+$router->get('transaction', ['uses' => 'Users\Wallets\Transactions\TransactionController@list']);
