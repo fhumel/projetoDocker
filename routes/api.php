e5639d9569ef35472a474cdaa3659da2dba465e2
Fernando Humel <fernando.procopio_artit@totalexpress.com.br>
2020-12-07 21:28:26 -0300

    ajuste de rotas

--- routes/api.php
+++ routes/api.php
@@ -3,23 +3,17 @@
 /** @var \Laravel\Lumen\Routing\Router $router */
 
 
-$router->get('/', function () use ($router) {
-    return $router->app->version();
-});
+$router->group(['prefix' => 'user'], function () use ($router) {
+    $router->post('register', ['uses' => 'Contracts/Users/UserControllerInterface@create']);
+    $router->get('list', ['uses' => 'Contracts/Users/UserControllerInterface@list']);
 
-$router->group(['prefix' => 'api'], function () use ($router) {
-    $router->group(['prefix' => 'user'], function () use ($router) {
-        $router->post('register', ['uses' => 'Contracts/Users/UserControllerInterface@create']);
-        $router->get('list', ['uses' => 'Contracts/Users/UserControllerInterface@list']);
+    $router->group(['prefix' => 'wallet'], function () use ($router) {
 
-        $router->group(['prefix' => 'wallet'], function () use ($router) {
+        $router->post('deposit', ['uses' => 'Contracts/Users/Wallets/WalletControllerInterface@deposit']);
+        $router->post('balance', ['uses' => 'Contracts/Users/Wallets/WalletControllerInterface@balance']);
 
-            $router->post('deposit', ['uses' => 'Contracts/Users/Wallets/WalletControllerInterface@deposit']);
-            $router->post('balance', ['uses' => 'Contracts/Users/Wallets/WalletControllerInterface@balance']);
-
-            $router->group(['prefix' => 'transaction'], function () use ($router) {
-                $router->post('list', ['uses' => 'Contracts/Users/Wallets/Transactions/TransactionControllerInterface@transaction']);
-            });
+        $router->group(['prefix' => 'transactions'], function () use ($router) {
+            $router->get('list', ['uses' => 'Contracts/Users/Wallets/Transactions/TransactionControllerInterface@transaction']);
         });
     });
 });
